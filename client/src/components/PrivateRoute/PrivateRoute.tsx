9c68c2d45f551205194222229773a8214d56ce6e
Minh Tran <mctran98@yahoo.com>
2021-07-13 14:55:46 -0700

    Seperated interface and assigned to props & used useAuth to check if user is logged in

--- client/src/components/PrivateRoute/PrivateRoute.tsx
+++ client/src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from '../../context/useAuthContext';
 
-const PrivateRoute: React.FC<{
+interface Props {
   component: React.FC;
   path: string;
   exact: boolean;
-  loggedIn: boolean;
-}> = (props) => {
-  const loggedIn = props.loggedIn;
-  return loggedIn ? (
+}
+
+const PrivateRoute:React.FC<Props> = (props:Props) => {
+  const authUser = useAuth();
+  return authUser.loggedInUser ? (
     <Route exact={props.exact} path={props.path} component={props.component} />
   ) : (
     <Redirect to="/login" />
