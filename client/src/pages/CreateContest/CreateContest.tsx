1b443e672812ebc38104ba0f15fa2c30587d3c55
Minh Tran <mctran98@yahoo.com>
2021-07-13 20:53:50 -0700

    Header and form skeleton

--- client/src/pages/CreateContest/CreateContest.tsx
+++ client/src/pages/CreateContest/CreateContest.tsx
@@ -1,7 +1,14 @@
 import CreateContestForm from './CreateContestForm/CreateContestForm';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 export default function CreateContest():JSX.Element {
   return (
-    <CreateContestForm />
+    <Box>
+      <Typography align="center" component="h1" variant="h5">
+        Create New Contest
+      </Typography>
+      <CreateContestForm />
+    </Box>
   );
 };
--- client/src/pages/CreateContest/CreateContestForm/CreateContestForm.tsx
+++ client/src/pages/CreateContest/CreateContestForm/CreateContestForm.tsx
@@ -1,8 +1,42 @@
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 export default function CreateContestForm():JSX.Element {
   return (
-    <div>
+    <Box>
+      <form>
+        <Box>
+          <Typography>What do you need designed?</Typography>
+          <TextField></TextField>
+        </Box>
+        <Box>
+          <Typography>Description</Typography>
+          <TextField></TextField>
+        </Box>
+        <Box>
+          <Box>
+            <Typography>Prize amount</Typography>
+            <TextField></TextField>
+          </Box>
+          <Box>
+            <Typography>Deadline</Typography>
+            <TextField></TextField>
+          </Box>
+        </Box>
+        <Box>
+          <Typography>Which designs do you like?</Typography>
+          <Box>
 
-    </div>
+          </Box>
+        </Box>
+        <Box>
+          <Button>
+
+          </Button>
+        </Box>
+      </form>
+    </Box>
   );
 };
