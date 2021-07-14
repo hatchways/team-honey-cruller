354bc631fe1788fbdba6231c91f226340f35afd2
jethoo <jeewanbhusal@gmail.com>
2021-07-14 12:45:39 -0400

    Created aws s3 bucket

--- server/routes/contest.js
+++ server/routes/contest.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const protect = require('../middleware/auth')
 const { createContest, updateContest, getSingleContest, getAllContests }  = require("../controllers/contest")
 
+const { submitPictures }  = require("../controllers/submission")
+
 router.route('/').post(protect,createContest).get(getAllContests)
 
 router.route("/:id").put(protect, updateContest).get(protect, getSingleContest)
 
+router.route("/:id/submission").post(protect, submitPictures)
 
 module.exports = router;
