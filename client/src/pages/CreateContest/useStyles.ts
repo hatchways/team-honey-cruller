8824fcf22d4dc03dcdc9e31bcf9ab3e7474cdd76
Minh Tran <mctran98@yahoo.com>
2021-07-14 19:30:43 -0700

    Implemented and styled create contest form

--- client/src/pages/CreateContest/useStyles.ts
+++ client/src/pages/CreateContest/useStyles.ts
@@ -1,9 +1,12 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => {
+const useStyles = makeStyles(() => ({
   root: {
 
+  },
+  header: {
+    fontWeight: 'bold',
   }
-});
+}));
 
 export default useStyles;
