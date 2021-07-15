ea82d902e7f9a4c50d3305fadee5973a57f09650
Minh Tran <mctran98@yahoo.com>
2021-07-15 15:23:51 -0700

    Added route to settings page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import SubmitDesign from './pages/SubmitDesign/SubmitDesign';
 import Signup from './pages/SignUp/SignUp';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Profile from './pages/Profile/Profile';
+import Settings from './pages/Settings/Settings'
 import { AuthProvider } from './context/useAuthContext';
 import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
@@ -24,6 +25,7 @@ function App(): JSX.Element {
                 <Route exact path="/signup" component={Signup} />
                 <Route exact path="/submit-design/:id" component={SubmitDesign} />
                 <Route exact path="/profile" component={Profile} />
+                <Route exact path="/settings" component={Settings} />
                 <Route exact path="/dashboard">
                   <Dashboard />
                 </Route>
