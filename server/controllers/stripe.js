62f4e0f3b43baabdcdee25ec083e0d4d8911e1b6
bf2344 <bford2344@gmail.com>
2021-07-17 17:03:42 -0600

    added atripe controllers and started on making front end fetches

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -4,8 +4,10 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 exports.createCustomer = asyncHandler(async (req, res) => {
     try {
         const customer = await stripe.customers.create({
-            email: req.body.email
-        })
+            email: req.body.email,
+            name: req.body.name
+        });
+
 
         res.status(200).json({
             customer: customer
@@ -16,13 +18,69 @@ exports.createCustomer = asyncHandler(async (req, res) => {
     }
 });
 
+exports.retrieveCustomer = asyncHandler(async (req, res) => {
+    try {
+        const customer = await stripe.customers.retrieve(req.params.id);
+
+        res.status(200).json({
+            customer: customer
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+exports.deleteCustomer = asyncHandler(async (req, res) => {
+    try {
+        const deleteCustomer = await stripe.customer.del(req.params.id);
+
+        res.status(200).json(deleteCustomer)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 exports.setUpIntents = asyncHandler(async (req, res) => {
     try {
-        const setupIntents = await stripe.setupIntents.create({});
-        const { client_secret } = setupIntents;
+        const setUpIntents = await stripe.setupIntents.create({});
+
         res.status(200).json({
-            clientSecret: client_secret
+            clientSecret: setUpIntents.client_secret
         });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+exports.createPaymentMethod = asyncHandler(async (req, res) => {
+    try {
+        const createPayment = await stripe.paymentMethods.create({
+            type: req.body.type,
+            // req.body.card should be an object with number, expMonth, expYear, and cvc inside.
+            card: req.body.card
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+exports.createCheckoutSession = asyncHandler(async (req, res) => {
+    try {
+        const checkoutSession = await stripe.checkout.sessions.create({
+            mode: 'subscription',
+            payment_method_types: ['card'],
+            line_items: [{
+                price: req.body.priceId,
+                // For metered billing, do not pass quantity
+                quantity: 1,
+            }, ],
+            success_url: 'https://example.com/success.html?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url: 'https://example.com/canceled.html'
+        })
+
+        res.status(200).json({
+            CheckoutSession: checkoutSession
+        })
     } catch (err) {
         res.status(500).json(err);
     }
--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../middleware/auth');
-const { createCustomer } = require('../controllers/stripe');
+const { createCustomer, setUpIntents } = require('../controllers/stripe');
 
 router.route('/').post(protect,createCustomer)
+
+router.route('/setup').post(protect,setUpIntents)
 
 module.exports = router
