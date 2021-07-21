920dfc8505e6046fb0e4fa64cdeee6a350f4a00d
Minh Tran <mctran98@yahoo.com>
2021-07-21 13:47:52 -0700

    Implemented a model for personal information

---
+++ server/models/PersonalInfo.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const PersonalInfoSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  firstName: {
+    type: String,
+  },
+  middleInit: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+  },
+  birthday: {
+    type: Date,
+  },
+  gender: {
+    type: String,
+  },
+  about: {
+    type: String,
+  },
+});
+
+module.exports = model('PersonalInfo', PersonalInfoSchema;)
