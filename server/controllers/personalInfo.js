089026efd943e26d6e3d5af41813ba9579e90444
Minh Tran <mctran98@yahoo.com>
2021-07-22 13:03:34 -0700

    Refactored personal info by spreading req body

--- server/controllers/personalInfo.js
+++ server/controllers/personalInfo.js
@@ -4,15 +4,7 @@ const asyncHandler = require("express-async-handler");
 exports.createPersonalInfo = asyncHandler(async (req, res) => {
   try {
     const info = PersonalInfo.create({
-      userId: req.user.id,
-      firstName: req.body.firstName,
-      middleInit: req.body.middleInit,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      phone: req.body.phone,
-      birthday: req.body.birthday,
-      gender: req.body.gender,
-      about: req.body.about
+      ...req.body
     });
     res.status(201).json(info);
   } catch (err) {
