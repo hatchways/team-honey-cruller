fcfbff4ce898fc7c88c0be4764b51cbfb41e7d20
Minh Tran <mctran98@yahoo.com>
2021-07-22 18:40:12 -0700

    Made images responsive, redirect to discovery page onSubmit, and use moment instead of date

--- client/src/pages/CreateContest/CreateContest.tsx
+++ client/src/pages/CreateContest/CreateContest.tsx
@@ -5,10 +5,13 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Contest } from '../../interface/Contest';
 import { addContest } from '../../helpers/APICalls/contest';
+import AuthHeader from '../../components/AuthHeader/AuthHeader';
+import { useHistory } from "react-router-dom";
 import useStyles from './useStyles';
 
 export default function CreateContest():JSX.Element {
   const classes = useStyles();
+  const history = useHistory();
 
   const handleSubmit = (
     {
@@ -23,25 +26,29 @@ export default function CreateContest():JSX.Element {
       title: title,
       description: description,
       prizeAmount: prizeAmount,
-      deadlineDate: deadlineDate,
+      deadlineDate: deadlineDate.format('MMMM Do YYYY h:mm A z'),
       images: images,
     };
 
     addContest(contest);
+    history.push('/discovery');
   };
 
   return (
-    <Grid container direction="column" spacing={0}>
-      <Box mt={4} mb={4}>
-        <Typography align="center" component="h1" variant="h5" className={classes.header}>
-          Create New Contest
-        </Typography>
-      </Box>
-      <Grid container justify="center">
-        <Grid item xs={12} md={10} elevation={6} component={Paper}>
-            <CreateContestForm handleSubmit={handleSubmit}/>
+    <>
+      <AuthHeader linkTo="/create-contest" btnText="create contest" />
+      <Grid container direction="column" spacing={0}>
+        <Box mt={4} mb={4}>
+          <Typography align="center" component="h1" variant="h5" className={classes.header}>
+            Create New Contest
+          </Typography>
+        </Box>
+        <Grid container justify="center">
+          <Grid item xs={12} md={10} elevation={6} component={Paper} className={classes.paper}>
+              <CreateContestForm handleSubmit={handleSubmit}/>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
