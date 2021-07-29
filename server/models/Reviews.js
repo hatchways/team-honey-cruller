45ff92ff4151f2f27b72652e73431a6e0f93755d
Minh Tran <mctran98@yahoo.com>
2021-07-28 23:04:50 -0700

    Changed schema

--- server/models/Reviews.js
+++ server/models/Reviews.js
@@ -1,31 +1,24 @@
 const { Schema, model } = require('mongoose');
 
-const ReviewsSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  reviews: [
-    {
-      reviewerId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-      rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-      text: {
-        type: String,
-      }
+const ReviewsSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
-  ]
-});
+    reviewerId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    text: {
+      type: String,
+    }
+  }, { timestamps: true });
 
 module.exports = Reviews = model('Reviews', ReviewsSchema);
