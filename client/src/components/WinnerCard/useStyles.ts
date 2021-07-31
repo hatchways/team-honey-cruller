d55dea11777ff92141536886f7f65b1d6da95daa
bf2344 <bford2344@gmail.com>
2021-07-31 15:51:10 -0600

    finished card

--- client/src/components/WinnerCard/useStyles.ts
+++ client/src/components/WinnerCard/useStyles.ts
@@ -1,7 +1,26 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  
+card: {
+    maxWidth: 345  
+},
+cardHeader: {
+
+},
+avatar: {
+
+},
+media: {},
+expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+    }),
+},
+expandOpen: {
+    transform: 'rotate(180deg)',
+},
 }));
 
 export default useStyles;
