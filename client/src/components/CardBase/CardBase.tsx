1f3425a0cb503c23dd41e21dd83d809af34aa660
bf2344 <bford2344@gmail.com>
2021-08-02 06:26:21 -0600

    added 2 components

---
+++ client/src/components/CardBase/CardBase.tsx
@@ -0,0 +1,57 @@
+import clsx from 'clsx';
+import { Card, CardContent } from '@material-ui/core';
+import useStyles from './useStyles';
+
+interface CardBaseProps {
+    className?: string;
+    children: JSX.Element;
+    withShadow?: boolean;
+    noShadow?: boolean;
+    noBorder?: boolean;
+    noBg?: boolean;
+    liftUp?: boolean;
+    align?: 'left' | 'right' | 'center';
+    cardContentProps?: object;
+    // All other props
+    [x: string]: any;
+}
+
+const CardBase = ({
+    withShadow,
+    noShadow,
+    noBorder,
+    noBg,
+    liftUp,
+    children,
+    align = 'center',
+    className,
+    cardContentProps = {},
+    ...rest
+}: CardBaseProps): JSX.Element => {
+    const classes = useStyles();
+
+    return (
+        <Card
+            className={clsx(
+                'card-base',
+                classes.root,
+                withShadow ? classes.withShadow : {},
+                noShadow ? classes.noShadow : {},
+                noBorder ? classes.noBorder : {},
+                noBg ? classes.noBg : {},
+                liftUp ? classes.liftUp : {},
+                className,
+            )}
+            {...rest}
+        >
+            <CardContent
+                className={clsx('card-base__content', classes.content, classes[align])}
+                {...cardContentProps}
+            >
+                {children}
+            </CardContent>
+        </Card>
+    );
+};
+
+export default CardBase;
--- client/src/pages/Discovery/Discovery.tsx
+++ client/src/pages/Discovery/Discovery.tsx
@@ -21,11 +21,9 @@ import TableCell from '@material-ui/core/TableCell';
 import TableBody from '@material-ui/core/TableBody';
 import TablePagination from '@material-ui/core/TablePagination';
 import Box from '@material-ui/core/Box';
-import Carousel from 'react-material-ui-carousel';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import IconButton from '@material-ui/core/IconButton'
 import { Animated } from 'react-animated-css';
-import withWidth, { WithWidth } from '@material-ui/core/withWidth';
 import Hidden from '@material-ui/core/Hidden';
 import WinnerCard from '../../components/WinnerCard/WinnerCard'
 import { CssBaseline } from '@material-ui/core';
@@ -110,23 +108,23 @@ export default function Discovery(): JSX.Element {
     }
   };
 
- const useWindowPosition = (id: string) => {
+  const useWindowPosition = (id: string) => {
     useLayoutEffect(() => {
-        function updatePosition() {
-            const offetSetHeight= ref.current && ref.current.offsetHeight;
-            if(offetSetHeight){
+      function updatePosition() {
+        const offetSetHeight = ref.current && ref.current.offsetHeight;
+        if (offetSetHeight) {
 
-              if (window.pageYOffset > offetSetHeight * 0.7) {
-                  setAnimation(true);
-              }
-            }
+          if (window.pageYOffset > offetSetHeight * 0.7) {
+            setAnimation(true);
+          }
         }
-        window.addEventListener('scroll', updatePosition);
-        updatePosition();
-        return () => window.removeEventListener('scroll', updatePosition);
-}, [id]);
-return animation;
-}
+      }
+      window.addEventListener('scroll', updatePosition);
+      updatePosition();
+      return () => window.removeEventListener('scroll', updatePosition);
+    }, [id]);
+    return animation;
+  }
 
   return (
     <>
@@ -139,31 +137,33 @@ return animation;
               <Typography variant='h1'>Welcome to Tatoo Art</Typography>
               <Typography variant='h5'>Premier tatoo designs created by artists all over the world.</Typography>
               <Scroll to='header' smooth={true}>
-              <IconButton>
-                <ExpandMoreIcon onClick={e => console.log(ref.current)} className={classes.expand} />
-              </IconButton>
+                <IconButton>
+                  <ExpandMoreIcon onClick={e => console.log(ref.current)} className={classes.expand} />
+                </IconButton>
               </Scroll>
             </Box>
           </Collapse>
         </Grid>
         <Grid ref={ref} id='header' className={classes.winnerCard}>
-        {winners.map((winner,i) => {
-          return (
-            <>
-            <WinnerCard
-            winningPic= {winner.winningPic}
-            title={winner.title}
-            prizeAmount={winner.prizeAmount}
-            winningArtist={winner.winningArtist}
-            description={winner.description}
-            key={winner.description}
-            time={1500}
-            useWindowPosition={useWindowPosition}
-            />
-            </>
-          )
-        })}
+          {winners.map((winner, i) => {
+            return (
+              <>
+                <WinnerCard
+                  winningPic={winner.winningPic}
+                  title={winner.title}
+                  prizeAmount={winner.prizeAmount}
+                  winningArtist={winner.winningArtist}
+                  description={winner.description}
+                  key={winner.description}
+                  time={1500}
+                  useWindowPosition={useWindowPosition}
+                />
+              </>
+            )
+          })}
         </Grid>
+
+        
         <Grid container justify="center" className={classes.grid}>
           <Grid>
             <Typography className={classes.typography}>All Open Contests</Typography>
