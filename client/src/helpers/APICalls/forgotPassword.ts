3d48cfd1049ce42184f2de7a75511f4f3e692c76
Minh Tran <mctran98@yahoo.com>
2021-08-04 14:36:05 -0700

    resetPassword helper function

--- client/src/helpers/APICalls/forgotPassword.ts
+++ client/src/helpers/APICalls/forgotPassword.ts
@@ -1,10 +1,14 @@
 import { AuthApiData } from '../../interface/AuthApiData';
 import axios from 'axios';
 
-const forgotPassword = async (email: string): Promise<AuthApiData> => {
+export const forgotPassword = async (email: string): Promise<AuthApiData> => {
   return axios.post('/reset', { email: email })
     .then((res) => res.data)
     .catch(() => ({error: {message: 'Cannot reach email'}}));
 };
 
-export default forgotPassword;
+export const resetPassword = async(password: string, token: string, id: string): Promise<AuthApiData> => {
+  return axios.patch(`/reset/update-password/${id}`, { password: password, token: token })
+    .then((res) => res.data)
+    .catch(() => ({error: {message: 'Unable to reset password'}}));
+};
