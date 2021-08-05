7d97dab6592b1400b35a9f51edbd50f91f38d27e
bf2344 <bford2344@gmail.com>
2021-08-05 16:35:20 -0600

    fixed merge issues

--- server/routes/contest.js
+++ server/routes/contest.js
@@ -4,7 +4,7 @@ const protect = require('../middleware/auth')
 const { createContest, updateContest, getSingleContest, getAllContests, chooseWinner, getNumContests }  = require("../controllers/contest")
 
 router.route('/').post(protect,createContest).get(getAllContests)
-router.route('/num').get(protect, getNumContests)
+router.route('/num').get(getNumContests)
 router.route("/:id").put(protect, updateContest).get(protect, getSingleContest).post(protect, chooseWinner)
 
 module.exports = router;
