3d6f2b2d821e1b760c42802ef97b108cf422452f
Minh Tran <mctran98@yahoo.com>
2021-08-05 18:15:18 -0700

    Added contest/:id path to if statement

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -52,7 +52,7 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
         } else {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
-          if (path.includes('/reset-password') || path.includes('/login') || path.includes('/forgot-password') || path.includes('/signup')) {
+          if (path.includes('/reset-password') || path.includes('/login') || path.includes('/forgot-password') || path.includes('/signup') || path.includes('/contest/:id')) {
             return;
           } else {
             history.push('/dashboard');
